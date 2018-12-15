0be49d68485fdb628de1cb52b5a7d1d13d023d5b
Thiago <thiago.adriano26@gmail.com>
2018-12-15 15:14:31 -0200

    add chatbot

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+var restify = require("restify");
+var builder = require("botbuilder");
+var five = require("johnny-five");
+
+var led;
+
+var board = new five.Board();
+board.on("ready", function() {
+  led = new five.Led(13);
+});
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
 
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-server.post('/api/messages', connector.listen());
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Você disse: %s", session.message.text);
+server.post("/api/messages", connector.listen());
+
+var bot = new builder.UniversalBot(connector, function(session) {
+  if (session.message.text == "ligar") {
+    led.on();
+  } else if (session.message.text == "desligar") {
+   led.off();
+  }
+});
+
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+  console.log("%s listening to %s", server.name, server.url);
 });
